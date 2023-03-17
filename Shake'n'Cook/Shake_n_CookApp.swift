f4f5ee385e5994900bffb4d5e42c73cae9fe6614
remybarbosa <barbosa.remy@gmail.com>
2023-03-17 07:25:32 +0100

    Initial Commit

---
+++ Shake'n'Cook/Shake_n_CookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shake_n_CookApp.swift
+//  Shake'n'Cook
+//
+//  Created by rémy barbosa on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Shake_n_CookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
